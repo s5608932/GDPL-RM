82f0cd03b6affede3df4b35301d35a3847f21e67
Rigle36 <s5608932@bournemouth.ac.uk>
2025-03-28 17:31:14 +0000

    Ball Power Limits Working
    Press Q or E to change power of the cannon

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class CannonController : MonoBehaviour
 {
     public float rotationSpeed = 1;
-    public float BlastPower = 5;
+    public float BlastPower = 25;
+    public int ShotCount = 10;
 
     public GameObject Cannonball;
     public Transform ShotPoint;
@@ -17,18 +18,41 @@ public class CannonController : MonoBehaviour
         float HorizontalRotation = Input.GetAxis("Horizontal"); // A or D / Left or Right
         float VerticalRotation = Input.GetAxis("Vertical"); // W or S / Up or Down
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            BlastPower -= 5;
+            if (BlastPower < 0)
+            {
+                BlastPower = 0;
+            }
+            
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            BlastPower += 5;
+            if (BlastPower > 50)
+            {
+                BlastPower = 50;
+            }
+        }
+
+
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, HorizontalRotation * rotationSpeed, VerticalRotation * rotationSpeed));
 
 
-
         //Fire
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && ShotCount > 0)
         {
+            ShotCount -= 1;
             GameObject CreatedCannonball = Instantiate(Cannonball, ShotPoint.position, ShotPoint.rotation);
             CreatedCannonball.tag = "Ball";
             CreatedCannonball.GetComponent<Rigidbody>().velocity = ShotPoint.transform.up * BlastPower;
             Destroy(CreatedCannonball, 10);
         }
+
+
+
+
 
     }
 
